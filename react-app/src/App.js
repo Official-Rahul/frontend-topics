1acb632845e52b3bbd568475b3612c4d411c1391
Rahul Singh <official.rahul.email@gmail.com>
2020-10-21 01:22:44 +0530

    react hooks useState and useEffect

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import ButtonComponent from './components/ButtonComponent';
+import ButtonFunctionComponent from './components/ButtonFunctionComponent';
 
 function App() {
+
+  const [flag, setFlag] = useState(true)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={()=>setFlag(!flag)}>Show/Hide</button>
+      {flag && <ButtonComponent />}
+      {flag && <ButtonFunctionComponent />}
     </div>
   );
 }
